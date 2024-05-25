40d359c5c6ced1e4c62b8411b4017133dc643822
Raidoyama <lahirulegend@gmail.com>
2024-05-25 13:51:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,8 +26,8 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 //💌------------------------------------------💌
 
 
-//💌global.pairingNumber = "" //put your bot number here💌
-global.mods = ['923042205427'] 
+//💌global.pairingNumber = "" /94724249733/put your bot number here💌
+global.mods = ['94724249733'] 
 global.prems = ['']
 global.allowed = ['']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
